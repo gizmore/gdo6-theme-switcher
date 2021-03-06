1279af459fde98b8ae19e1522b529dc0cd49dcfc
gizmore <gizmore@wechall.net>
2021-03-06 19:04:43 +0100

    6.10.1-r1009_unit_tests_and_docs

--- Method/Themes.php
+++ Method/Themes.php
@@ -6,14 +6,26 @@ use GDO\ThemeSwitcher\GDO_Theme;
 use GDO\ThemeSwitcher\Module_ThemeSwitcher;
 use GDO\Core\MethodAdmin;
 
+/**
+ * List all theme switcher themes.
+ * Very common and easy task to render a list out of a db.
+ * @author gizmore
+ * @version 6.10.1
+ * @since 6.10.0
+ */
 final class Themes extends MethodQueryList
 {
-    use MethodAdmin;
-    
+    use MethodAdmin; # default admin method permission stuff and navbar rendering
+
+    /**
+     * Rende
+     * {@inheritDoc}
+     * @see \GDO\Core\Method::beforeExecute()
+     */
     public function beforeExecute()
     {
-        $this->renderNavBar();
-        Module_ThemeSwitcher::instance()->renderAdminTabs();
+        $this->renderNavBar(); # admin navbar
+        Module_ThemeSwitcher::instance()->renderAdminTabs(); # switcher navbar
     }
     
     public function gdoTable()
