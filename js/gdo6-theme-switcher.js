97b475ac6442e71bdac7ddc4a925d93e965aa244
gizmore <gizmore@wechall.net>
2021-05-06 01:24:15 +0200

    6.10.2-r1071

--- js/gdo6-theme-switcher.js
+++ js/gdo6-theme-switcher.js
@@ -7,7 +7,7 @@ var nodes = document.querySelectorAll('.gdo-theme-switcher select');
 var len = nodes.length;
 for (var i = 0; i < len; i++) {
 	nodes[i].addEventListener('change', function(e){
-		var url = GWF_WEB_ROOT + 'index.php?mo=ThemeSwitcher&me=Set&theme=' + this.value + "&ref=" + encodeURIComponent(window.location.href);
+		var url = GDO_WEB_ROOT + 'index.php?mo=ThemeSwitcher&me=Set&theme=' + this.value + "&ref=" + encodeURIComponent(window.location.href);
 		window.location.href = url;
 	}); 
 }
